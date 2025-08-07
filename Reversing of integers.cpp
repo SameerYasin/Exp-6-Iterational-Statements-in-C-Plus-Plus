804d63e418c630bc437cfffd91e39a5270a182f4
Sameer Yasin <113780206+SameerYasin@users.noreply.github.com>
2025-08-07 12:17:45 +0530

    Create Reversing of integers.cpp

---
+++ Reversing of integers.cpp
@@ -0,0 +1,21 @@
+//Sameer Yasin Exp-6
+//24070123092
+#include<iostream>
+using namespace std;
+int main(){
+    int n, x, y, s = 0;
+    cout << "Enter your PRN/Roll number: ";
+    cin >> n;
+    while(n > 0){
+        x = n % 10;
+        s = s * 10 + x;
+        n = n / 10;
+    }
+    cout << s;
+}
+/*
+Output:
+Enter your PRN/Roll number: 135
+531
+*/
+
