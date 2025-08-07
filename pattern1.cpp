6153f0b251e2121149301938eda04606eef2519c
Sameer Yasin <113780206+SameerYasin@users.noreply.github.com>
2025-08-07 12:14:29 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,22 @@
+//Sameer Yasin Exp-7
+//24070123092
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j;
+    int n = 5;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+/*
+Output:
+*
+**
+***
+****
+*****
+*/
