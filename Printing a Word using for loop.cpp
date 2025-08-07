a704648af2bf383ecf05b7b788ba19ef2abf7b43
Sameer Yasin <113780206+SameerYasin@users.noreply.github.com>
2025-08-07 12:13:09 +0530

    Create Printing a Word using for loop.cpp

---
+++ Printing a Word using for loop.cpp
@@ -0,0 +1,19 @@
+//Sameer Yasin Exp-6
+//24070123092
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        cout << "Sit" << endl;
+    }
+}
+/*
+Output:
+Sit
+Sit
+Sit
+Sit
+Sit
+*/
